17b216027cc90f8bed292f9b1a244a6830f02663
Tama <tamagawayuuma@icloud.com>
2021-05-03 18:07:05 +0900

    block

---
+++ Assets/Scripts/itemblock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemblock : MonoBehaviour
+{
+    public GameObject blockprefab;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "player")
+        {
+            Vector2 vec = this.gameObject.transform.position;
+            Destroy(this.gameObject);
+            Instantiate(blockprefab, vec, Quaternion.identity);
+        }
+    }
+}
