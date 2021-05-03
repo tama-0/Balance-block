a4f8586753897d86ab24cd23afeed0fa1111b06b
Tama <tamagawayuuma@icloud.com>
2021-05-03 14:20:18 +0900

    stage&player

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+	public GameObject player;
+	public float hight = 5.0f;
+	public float pos = -6.5f;
+
+	void Start()
+	{
+
+	}
+
+
+	void Update()
+	{
+		Vector3 playerpos = this.player.transform.position;
+		transform.position = new Vector3(playerpos.x, playerpos.y + hight, playerpos.z + pos);
+	}
+}
